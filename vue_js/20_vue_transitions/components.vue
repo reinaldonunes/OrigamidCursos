5f48f161c96d70f2e2dcd9f0c9c3e0e0486ced05
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-10-11 16:52:31 -0300

    Feat: add transitions properties

---
+++ vue_js/20_vue_transitions/components.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="estoque++">Adicionar Estoque</button>
+    <button @click="estoque = 0">Deletar Estoque</button>
+
+    <transition name="fade" mode="out-in">
+      <p v-if="estoque > 0" key="estoque">{{ estoque }} itens em estoque.</p>
+      <p v-else key="esgotado">Produto fora de estoque.</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+import transition from './transition.vue'
+export default {
+  data(){
+    return{
+      estoque: 0
+    }
+  },
+  components: { transition },
+
+}
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active{
+  transition: opacity .3s;
+}
+
+.v-enter,
+.v-leave-to{
+  opacity: 0;
+}
+
+</style>
---
+++ vue_js/20_vue_transitions/transition.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Ativar</button>
+    <transition
+      name="fade"
+      enter-active-class="showing"
+      enter-to-class="animated"
+    >
+      <h1 v-if="ativo">Serviços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      ativo: false
+    }
+  }
+}
+</script>
+
+<style>
+  .v-enter-active,
+  .v-leave-active{
+    transition: opacity .3s;
+  }
+
+  .showing{
+    transition: opacity .3s;
+  }
+  .animated{
+    color:rgb(150, 0, 0);
+  }
+
+  .v-enter,
+  .v-leave-to{
+    opacity: 0;
+  }
+</style>
